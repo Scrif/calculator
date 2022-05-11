2b8c8724a6bc9fda0b8759345f7f2f3007eed7a5
scrif <andrews.austin.94@gmail.com>
2022-05-11 13:43:39 -0500

    Finished operate function
    Takes two variables and an operator.
    Uses switch to choose a case based on the operator, then calls the appropriate function
    to do math on the input variables. Returns the output of the math function.

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ const subtract = (a,b) => a - b;
 const multiply = (a,b) => a * b;
 const divide = (a,b) => a / b;
 
-const operate(a, operator, b) {
-    
-}
+function operate(a, operator, b) {
+    a = Number(a);
+    b = Number(b);
+    switch (operator) {
+        case '+':
+            return add(a,b)
+        case '-':
+            return subtract(a,b)
+        case '*':
+            return multiply(a,b)
+        case '/':
+            if (b === 0) return null    
+            else return divide(a,b)
+        default:
+            return null
+    }
+};
